9d07d1166bfa772534431f1236e73b56a0beec22
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-10-29 14:13:51 +0300

    Update welcome-page.component.ts

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,7 +1,7 @@
 
-import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+/*import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
-import { MovieCardComponent } from '../movie-card/movie-card.component';
+//import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
@@ -26,10 +26,40 @@ export class WelcomePageComponent implements OnInit {
     });
   }
 
-  openMoviesDialog(): void {
-    this.dialog.open(MovieCardComponent, {
-      width: '500px'
+}*/
+
+import { Component, OnInit } from '@angular/core';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form.component';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss']
+})
+export class WelcomePageComponent implements OnInit {
+  constructor(public dialog: MatDialog) { }
+  ngOnInit(): void {
+  }
+
+  /**
+ * opens the user registration dialog when signup button is clicked
+ */
+
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+
+  /**
+ * opens the user login dialog when login button is clicked
+ */
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
     });
   }
 }
-
