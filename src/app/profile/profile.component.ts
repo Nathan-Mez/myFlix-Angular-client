79782b6a30d6cce2fff84d38bf2c8ccb9b7e49d1
Nathan-Mez <106163966+Nathan-Mez@users.noreply.github.com>
2022-11-02 10:18:07 +0300

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  user: any = {};
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+  ) {}
 
   ngOnInit(): void {
+    this.getUser();
   }
 
-}
+  /**
+   * Gets user data from api call and sets the user variable to returned JSON file
+   * @returns object holding user information
+   * @function getUser
+   */
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.user = resp;
+      console.log(this.user);
+      return this.user;
+    });
+  }
+
+    }
